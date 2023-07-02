466c9165f4c25563f93272e073dae78df710e2a9
Dishant Gupta <84134946+Dishant-jpg@users.noreply.github.com>
2023-07-03 00:21:05 +0530

    ,h

---
+++ learning/btn.js
@@ -0,0 +1,8 @@
+let namastebtn = document.querySelector('button');
+namastebtn.addEventListener('click', inputmsg);
+
+function inputmsg(){
+    let name = prompt('Enter Name of Student');
+    namastebtn.textContent = 'Roll No. 1:' + name ;
+    // alert("High issue");
+}
